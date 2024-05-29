a88a2295fb12a1612ea2e0b46e899a8c7be9f256
Sandra Moreno <smoreno@robotnik.es>
2024-05-29 10:33:55 +0200

    Added a timer

--- src/pose_publisher.cpp
+++ src/pose_publisher.cpp
@@ -1,13 +1,33 @@
+#include <chrono>
+
 #include "rclcpp/rclcpp.hpp"
+
+using namespace std::chrono_literals;
 
 class PosePubNode : public rclcpp::Node
 {
 public:
     PosePubNode() : Node("node_pose_publisher") {
         RCLCPP_INFO(get_logger(),  "Connection established");
+
+        // std::chrono::seconds timer_frequency(1);
+        std::chrono::milliseconds timer_frequency{500};
+
+        // Call timer_callback function every timer_frequency
+        timer_ = this->create_wall_timer(
+            timer_frequency, std::bind(&PosePubNode::timer_callback, this));
     }
 
+private:
+    void timer_callback() {
+        RCLCPP_INFO(get_logger(),  "In timer_callback");
+    }
+
+    /* Here it is created the shared pointers to the Publisher and timer objects
+    * defined above, and it is also created the variable count*/
+    rclcpp::TimerBase::SharedPtr timer_;
 };
+
 
 int main(int argc, char **argv)
 {
